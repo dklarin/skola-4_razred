2a4b26917cc208faf4bf8073a7aa58334c373df3
dklarin <danijel.klarin@gmail.com>
2025-04-13 11:51:47 +0200

    first commit

---
+++ links.py
@@ -0,0 +1,78 @@
+links_it = {    
+    '2.1. Prva pomagala za računanje': 'ERrqltsrgQtMqbFEOu4r_gwBeG05XcpWnAOzKPRJUYxUXQ',
+    '2.2. Mehanički strojevi': 'EW6XoAHCgrJJp53EbHc-5-sBENP-lA0B1wwzxaK7uTl6Uw',
+    '2.3. Elektromehanički strojevi': 'EaeZEZvvU9JBsAwisI63UTwBuOdU-BMPv0H-JDLGT-aVzw',
+    '2.4. Elektronička računala': 'EfLHXHT6SL1Mg6vP4w7aXL8BVyg_q_uA_iS2MymJDTI6LQ',
+    '2.5. Osobna računala': 'EfAZqHYJrE9Cjkf9ditARlEBzd7joQCuv2Uqr5-QRq1zFg',
+    '3.1. Računalno sklopovlje': 'EYz6AdfpYlFBjw9zlixE0L0B5ULEP8PuzATahSfSMHT3jA',
+    '3.2. Glavni dijelovi računala': 'Edk705kaRzBBvPnYdZvs1NYByTCQOA7Dij1Ja8y4Jab4Eg',
+    '4.5. Autorska prava i distribucija programa': 'EfC_38Iy4eFLunHQzdeKrsgBL1j3K_E2er55nKsmYO0z0A',
+    '4.6. Zlonamjerni programi': 'EZD56h3fcd9Dvce5tzY2-sgBeG1dUZUJCBFX8VK46K9Xnw',
+    '4.7. Pričuvna pohrana podataka': 'EcIEdV7OifdBtiKsUiVx888BAbz7_dTjQWKUGNl0gCflLA'
+}
+
+software = {
+    'LatencyMon 7.31 (Home Edition)': 'EdYlk4AUohBDvmbQZSBwhDUBolAQTunb4ME31mcWn_BY9g'
+}
+
+
+links = {    
+    '1. Uvod': 'ESjQtcFLbsJOnthinieDf0oBSKcs5uW_vL9TadUCX8avXQ',
+    '2. Pregled operacijskih sustava': 'Ect-CYUsa-9Npuwo0yQuxvMBUs5M6Vdrv2AgvmWAUk3Hmg',
+    '3. Pokretanje': 'EXTKN7RuKKtIlpGus0GUyzsBOCQFOYwl64c6bqbuD-LMiw',
+    '4. MBR, Particije': 'EeJbJkwhqF9EiubGYPLHL-ABYDQh-gIawdFMH59lruZQcQ',
+    '5. Načelo prekida, Dodjeljivač sabirnice, Zahtjev-prihvat, Vektor prekida': 'EeKg_7A-TW1Cp8nw7P8Py2cBxEJ2qi2rDsnnf2Gz3Zj_Fw',
+    '6. Prioritet prekida, Programski prekidi': 'EcnuvNGqQolHuclIkDsD7UwB5ASsPrBx6XXigbQjW7TUag',
+    '7. Program, proces, dretva, raspoređivanje po vremenu i prioritetu ': 'EXHhDnOEyC1GuNbQUEii8H4BM2LF5Smkf4OVMy9HMjIgAw'   
+}
+
+# Prezentacije učenika
+links_sjwp_u = {    
+    '1. Flexbox u CSS-u': 'Eaz2wjZ3oJ1LmyLKWPZXSgQB8cdoSxOpqFJiOb74PScJjw?e=hQhDC8',
+    '2. HTML': 'ESd-HSjMVspDkrZoauuS8BMB2PeBkb5upkj3NlUUc1ooFg?e=MYnOcs',
+    '3. Općenito o CSS-u': 'EX_ZjWSlj2dCm9Y0p396cP4BoNrDer4GpFVVBhry7uPFoA?e=YLUfoM'
+}
+
+links_sjwp_1_kodovi = { 
+    'Oblikovanje teksta': 'EawhO-ShpfhDkg2aGlLyhm8BijRizwXJbcZxA3pupSuKdQ?e=hrRQHL',
+    'Poveznice': 'EeW38fY2R1FCkIZwK6-pbXIB0zHrH1D-YNgaDc3w_Mx-4A?e=VuiLz7',
+    'Tablice': 'ET3Zx8QirylJkBhmav3PBdkByzXjU-yJ-jP1Uog-tzCCBw?e=wHV5cm',
+    'Slike': 'Ec-AO99Ag11CsVYes47XJzUBua34eCLwl8rwvaXxtSBRlg?e=SXm950',
+    'Liste': 'ESQ3-rxkN3RHiLZjVu_kdVcBIDeS0MTuTnyvm0yg0s1KYw?e=ZGEhbU',
+    'Oblikovanje mrežnog sjedišta s pomoću HTML-a': 'ETAlsrndsxdHjnt5qVk4GTABmniGa10kn-WSOFACuuqULA?e=OOiF83'
+}
+
+links_sjwp_1_literatura = {    
+    'Ne radi još': 'EeW38fY2R1FCkIZwK6-pbXIB0zHrH1D-YNgaDc3w_Mx-4A?e=VuiLz7',
+    'Ne radi još': 'ET3Zx8QirylJkBhmav3PBdkByzXjU-yJ-jP1Uog-tzCCBw?e=wHV5cm',
+    'Ne radi još': 'Ec-AO99Ag11CsVYes47XJzUBua34eCLwl8rwvaXxtSBRlg?e=SXm950',
+    'Oblikovanje teksta': 'ESFgmyg1zExPoEP9weyUcWwBX39WGAR17UntgQWVAyQyQA?e=6Osq01',
+    'Oblikovanje mrežnog sjedišta s pomoću HTML-a': 'EabNYYBkIc5Nhh5L845XWAgBPujKKxz5lRe6YlDAobtReA?e=cVo405'
+}
+
+flask = {
+    '1. Prvi projekt': 'ETehfSJLnjROuFpGUt0ZWcsBGMhwJYFjHkmkmrK4Z_n4vg?e=8pBKbq',
+    '3. Templates 2': 'Edo8EuhqmaZIlbuZ_V1W780Bq8dxj4kVLTWmB_szxdX7OQ'
+}
+
+web_dizajn = {
+    '1. Flask 1: Dinamička ruta, if-else i for': 'Efpp7buw9NhHtZxXixSblXAB7jIzAqIa-dCLDwsGMJ7Ymw?e=fg0KLO'
+}
+
+links_web_dizajn_3 = {
+    '1.1.3. Upoznavanje sa sučeljem': 'EWxGBt47OhpPp1Eazt8aEzABeEtcUacjiaPI3vo0GKo4fw',
+    '1.2. Izrada novog dokumenta': 'EYhn-L3DcjRHktX8kg6YrDcB5eUwSGaEjXIv72RbGUoYDQ'
+}
+
+links_react_native = {
+    '0. Što je React Native': 'EVqOlaAn11JNgHx0o_11MnEBIkK07rwEJxJCPhwQzW0MgQ',
+    '1. Programska podrška za mobilne uređaje': 'EdbRmq9qCftDqYVddVYa9ZUBQcaqSR8VJOZGjfLWYQU0uA'
+}
+
+links_pmu_2_kodovi = { 
+    'Flask-Server': 'ERqZL77q0_VKk4BX1Rrn080BZCpTaWhUm0qTSf43rpUZHg?e=SYPuRD'  
+}
+
+links_pmu_2_literatura = {    
+    'Server-Klijent': 'Edtdj3H6WfNFk_NLjm9u8zQBPpaqFCPUOvBax0WA8OP5lw?e=UYCma3'    
+}
