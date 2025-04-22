ea58b649bc6630c4b630bf833163173c4bc990d0
dklarin <danijel.klarin@gmail.com>
2025-04-22 15:51:25 +0200

    sitne promjene

--- app.py
+++ app.py
@@ -24,42 +24,12 @@ def index():
 def frame():
     return render_template('frame.html')
 
-# flask-bootstrap
-@app.route('/it')
-def informacijske_tehnologije():
-    title = 'Informacijske tehnologije'
-    return render_template('index.html', var=var, title=title, links_it=links_it)
+link1 = '/pmu_2'
+link2 = '/wd_2'
 
-link1 = '/opsus'
-link2 = '/'
-link3 = '/pmu_2'
-link4 = '/wd_1'
+subject1 = 'Programiranje mobilnih uređaja 2'
+subject2 = 'Web dizajn 2'
 
-subject1 = 'Ne radi još' #'Operacijski sustavi'
-subject2 = 'Ne radi još' #Skriptni jezici i web programiranje 1
-subject3 = 'Programiranje mobilnih uređaja'
-subject4 = 'Ne radi još' #'Web dizajn 1'
-
-# 3. razred
-# flask-bootstrap
-@app.route('/opsus')
-def operacijski_sustavi():
-    title = 'Operacijski sustavi'  
-    return render_template('index.html', 
-                            var=var,
-                            title=title, 
-                            link1=link1, 
-                            link2=link2, 
-                            link3=link3, 
-                            link4=link4,
-                            subject1=subject1,
-                            subject2=subject2,
-                            subject3=subject3,
-                            subject4=subject4,
-                            links_it=links
-                            )
-
-# flask-bootstrap
 @app.route('/')
 def skriptni_jezici_i_web_programiranje_1():
     title = 'Skriptni jezici i web programiranje 1'    
@@ -67,52 +37,39 @@ def skriptni_jezici_i_web_programiranje_1():
                             var=var, 
                             title=title, 
                             link1=link1, 
-                            link2=link2, 
-                            link3=link3, 
-                            link4=link4,
+                            link2=link2,                             
                             subject1=subject1,
-                            subject2=subject2,
-                            subject3=subject3,
-                            subject4=subject4,
+                            subject2=subject2,                          
                             links_it=links_sjwp_1_kodovi,
                             links_lit=links_sjwp_1_literatura
                             )
 
-# flask-bootstrap
-@app.route('/wd_1')
-def web_dizajn_1():
-    title = 'Web dizajn'   
-    return render_template('index.html', 
-                            var=var, 
-                            title=title, 
-                            link1=link1, 
-                            link2=link2, 
-                            link3=link3, 
-                            link4=link4,
-                            subject1=subject1,
-                            subject2=subject2,
-                            subject3=subject3,
-                            subject4=subject4,
-                            links_it=links_web_dizajn_3
-                            )
-
-# flask-bootstrap
 @app.route('/pmu_2')
 def pmu_2():
-    title = 'Programiranje mobilnih uređaja'   
+    title = 'Programiranje mobilnih uređaja 2'   
     return render_template('index.html', 
                             var=var, 
                             title=title, 
                             link1=link1, 
-                            link2=link2, 
-                            link3=link3, 
-                            link4=link4,
+                            link2=link2,                           
                             subject1=subject1,
-                            subject2=subject2,
-                            subject3=subject3,
-                            subject4=subject4,
+                            subject2=subject2,                          
                             links_it=links_pmu_2_kodovi,
                             links_lit=links_pmu_2_literatura
+                            )
+
+@app.route('/wd_2')
+def wd_2():
+    title = 'Web dizajn 2'   
+    return render_template('index.html', 
+                            var=var, 
+                            title=title, 
+                            link1=link1, 
+                            link2=link2,                            
+                            subject1=subject1,
+                            subject2=subject2,                           
+                            links_it=links_wd_2_kodovi,
+                            links_lit=links_sjwp_1_literatura
                             )
 
 if __name__ == "__main__":
